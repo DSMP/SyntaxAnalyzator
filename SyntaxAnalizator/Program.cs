44c7d866e74cc28763f0e6aa305bbe4fcfc546ca
damian <damiakuz@wp.pl>
2018-06-19 20:32:09 +0200

    improve efficiency of analizator

--- SyntaxAnalizator/Program.cs
+++ SyntaxAnalizator/Program.cs
@@ -7,7 +7,7 @@ namespace SyntaxAnalizator
     {
         static void Main(string[] args)
         {
-            SyntaxStructure syntaxStructure = new SyntaxStructure("(-1.2*3)+5-(23.4+3000*(22*12-(2+2)))^3;8:13;".ToCharArray());
+            SyntaxStructure syntaxStructure = new SyntaxStructure("(1.2*3)+5-(23.4+3)^3;8:13;".ToCharArray());
             var head = syntaxStructure.Head;
             int i = 0;
             try
@@ -15,7 +15,7 @@ namespace SyntaxAnalizator
                 while (SyntaxStructure._iterator != SyntaxStructure.Syntax.Length - 1)
                 {
                     head = head.Next();
-                    //Console.WriteLine(i++ + " " + SyntaxStructure._iterator + " " + head.Value.ToString() + " " + SyntaxStructure.Syntax[SyntaxStructure._iterator]);
+                    Console.WriteLine(i++ + " " + SyntaxStructure._iterator + " " + head.Value.ToString() + " " + SyntaxStructure.Syntax[SyntaxStructure._iterator]);
                 }
             }
             catch (NullReferenceException)
@@ -23,7 +23,7 @@ namespace SyntaxAnalizator
                 Console.WriteLine("Składnia jest nie poprawna, na pozycji {0} wystąpił nie poprawny symbol: \"{1}\"", SyntaxStructure._iterator, SyntaxStructure.Syntax[SyntaxStructure._iterator]);
                 return;
             }
-            Console.WriteLine("skłądnia jest poprawna");
+            Console.WriteLine("składnia jest poprawna");
         }
     }
 }
--- SyntaxAnalizator/SyntaxStructure.cs
+++ SyntaxAnalizator/SyntaxStructure.cs
@@ -22,19 +22,39 @@ namespace SyntaxAnalizator
             Strukturka siedem = new Strukturka();
             Strukturka osiem = new Strukturka();
             Strukturka dziewiec = new Strukturka();
+
+            Strukturka zero1 = new Strukturka();
+            Strukturka jeden1 = new Strukturka();
+            Strukturka dwa1 = new Strukturka();
+            Strukturka trzy1 = new Strukturka();
+            Strukturka cztery1 = new Strukturka();
+            Strukturka piec1 = new Strukturka();
+            Strukturka szesc1 = new Strukturka();
+            Strukturka siedem1 = new Strukturka();
+            Strukturka osiem1 = new Strukturka();
+            Strukturka dziewiec1 = new Strukturka();
+
             Strukturka kropka = new Strukturka();
             Strukturka mnoz = new Strukturka();
             Strukturka dziel = new Strukturka();
             Strukturka dodaw = new Strukturka();
             Strukturka odej = new Strukturka();
             Strukturka pot = new Strukturka();
+
+            Strukturka mnoz1 = new Strukturka();
+            Strukturka dziel1 = new Strukturka();
+            Strukturka dodaw1 = new Strukturka();
+            Strukturka odej1 = new Strukturka();
+            Strukturka pot1 = new Strukturka();
+
             Strukturka srednik = new Strukturka();
             Strukturka pusty = new Strukturka();
             Strukturka error = new Strukturka();
 
             S.InitStrukturka(nawO, nawO, '\n');
             nawO.InitStrukturka(zero, zero, '(');
-            nawZ.InitStrukturka(mnoz, pusty, ')');
+            nawZ.InitStrukturka(mnoz, mnoz1, ')');
+
             zero.InitStrukturka(kropka, jeden, '0');
             jeden.InitStrukturka(kropka, dwa, '1');
             dwa.InitStrukturka(kropka, trzy, '2');
@@ -44,14 +64,33 @@ namespace SyntaxAnalizator
             szesc.InitStrukturka(kropka, siedem, '6');
             siedem.InitStrukturka(kropka, osiem, '7');
             osiem.InitStrukturka(kropka, dziewiec, '8');
-            dziewiec.InitStrukturka(kropka, mnoz, '9');
+            dziewiec.InitStrukturka(kropka, null, '9');
+
+            zero1.InitStrukturka(srednik, jeden1, '0');
+            jeden1.InitStrukturka(srednik, dwa1, '1');
+            dwa1.InitStrukturka(srednik, trzy1, '2');
+            trzy1.InitStrukturka(srednik, cztery1, '3');
+            cztery1.InitStrukturka(srednik, piec1, '4');
+            piec1.InitStrukturka(srednik, szesc1, '5');
+            szesc1.InitStrukturka(srednik, siedem1, '6');
+            siedem1.InitStrukturka(srednik, osiem1, '7');
+            osiem1.InitStrukturka(srednik, dziewiec1, '8');
+            dziewiec1.InitStrukturka(srednik, null, '9');
+
             mnoz.InitStrukturka(nawO, dziel, '*');
             dziel.InitStrukturka(nawO, dodaw, ':');
             dodaw.InitStrukturka(nawO, odej, '+');
             odej.InitStrukturka(nawO, pot, '-');
-            pot.InitStrukturka(nawO, srednik, '^');
-            srednik.InitStrukturka(nawO, nawZ, ';');
-            kropka.InitStrukturka(nawO, zero, '.');
+            pot.InitStrukturka(nawO, nawZ, '^');
+
+            mnoz1.InitStrukturka(nawO, dziel1, '*');
+            dziel1.InitStrukturka(nawO, dodaw1, ':');
+            dodaw1.InitStrukturka(nawO, odej1, '+');
+            odej1.InitStrukturka(nawO, pot1, '-');
+            pot1.InitStrukturka(nawO, zero, '^');
+
+            srednik.InitStrukturka(nawO, mnoz, ';');
+            kropka.InitStrukturka(zero1, srednik, '.');
             pusty.InitStrukturka(null, error, 'E');
             error.InitStrukturka(null, null, '@');
             Syntax = syntax;
